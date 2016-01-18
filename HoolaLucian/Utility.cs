542c660f6ab91e84abe62fe150c205065794b7fa
ThugDoge <welington_aquino@hotmail.com>
2016-01-18 06:14:02 -0200

    Doge

---
+++ HoolaLucian/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy.SDK;
+using SharpDX;
+
+namespace HoolaLucian
+{
+    static class Utility
+    {
+        public static Vector3 Extend(this Vector3 vector3, Vector3 toVector3, float distance)
+        {
+            return vector3 + (distance * (toVector3 - vector3).Normalized());
+        }
+    }
+}
